7020d85437c268225812ded2a90fc565089eb06d
FSdev <sreerammanjunath1392@gmail.com>
2022-03-20 17:32:41 +0530

    Version 1 commit -m

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; 
+import { FontAwesome } from '@expo/vector-icons'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+   
+    <View style={styles.navbar}>
+
+<View>
+<Ionicons name="menu-sharp" size={35} color="black" />
+</View>
+
+<View>
+<FontAwesome name="search" size={30} color="black" />
+</View>
+
+    </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +28,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+
+  navbar: {
+    marginTop: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    height: 60,
+   backgroundColor: '#f2f2f2',
   },
 });
