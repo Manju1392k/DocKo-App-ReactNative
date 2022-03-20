40ddb30a9b5d4769820acb9c9f5836752461b441
FSdev <sreerammanjunath1392@gmail.com>
2022-03-20 17:45:27 +0530

    Version 2 commit -m

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'; 
-import { FontAwesome } from '@expo/vector-icons'; 
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View,ScrollView } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-   
-    <View style={styles.navbar}>
+      <View style={styles.navbar}>
+        <View>
+          <Ionicons name="menu-sharp" size={35} color="black" />
+        </View>
 
-<View>
-<Ionicons name="menu-sharp" size={35} color="black" />
-</View>
+        <View>
+          <FontAwesome name="search" size={30} color="black" />
+        </View>
 
-<View>
-<FontAwesome name="search" size={30} color="black" />
-</View>
+      </View>
+
+      <ScrollView>
+  <View style={styles.backforimage}>
+
+    <View style={styles.infobox}>
+    <Text style={styles.textheading}>Docko MultiSpecial Hospital</Text>
+    <Text style={styles.textheading}>Best Experience Medical Industry</Text>
+    <Text style={styles.textheading}>OverAll 20 Years Plus Experience Doctors Available</Text>
+
 
     </View>
+
+  </View>
+</ScrollView>
 
       <StatusBar style="auto" />
     </View>
@@ -27,16 +39,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 
   navbar: {
     marginTop: 40,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    width: "100%",
     height: 60,
-   backgroundColor: '#f2f2f2',
+    backgroundColor: "#f2f2f2",
   },
+
+  backforimage: {
+    width: '100%',
+    height: 250,
+    flexDirection: 'row',
+    justifyContent: "center",
+    alignItems: 'center',
+  },
+
+  infobox: {
+    width: '80%',
+    height: 200,
+    backgroundColor: '#00CCFF',
+    borderRadius: 10,
+    padding: 20,
+  },
+
+  textheading: {
+    fontSize: 20,
+    marginTop: 8,
+    fontWeight: 'bold',
+  }
 });
